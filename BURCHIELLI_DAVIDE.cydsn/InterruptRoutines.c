d7f0df58d2ecefac7fdc8decba9d6d2544e3a935
Davide <burchielli.davide@gmail.com>
2020-10-26 23:33:46 +0100

    InterruptRoutines files commented

--- BURCHIELLI_DAVIDE.cydsn/InterruptRoutines.c
+++ BURCHIELLI_DAVIDE.cydsn/InterruptRoutines.c
@@ -2,74 +2,80 @@
     author: Davide Burchielli
 
     This file defines the functions and the ISRs.
-    Custom_UART_ISR is executed everytime the isr_UART is triggered by the arrival of a byte. 
-    Custom_TIMER_ISR is executed everytime the isr_TIMER is triggered by a TC event, so every 5 seconds.  
+    Custom_UART_RX_ISR is executed everytime the isr_UART_RX is triggered by the arrival of a byte. 
+    Custom_TIMER_ADC_ISR is executed everytime the isr_TIMER_ADC is triggered by a TC event, so every 5 seconds.  
  * ======================================================================================================
 */
 
 #include "project.h"
 #include "InterruptRoutines.h"
 
-int32 data_digit;
-int32 data_mV;
 
-void Start_HW_Components()
+int32 read_data;  // read_data variable contains the value read from the ADC
+
+void Start_HW_Components()   // This function starts the hardware components: MUX, ADC and Timer
 {  
-    AMUX_Start();
-    AMUX_FastSelect(PHOTOR);
-    ADC_DelSig_Start();
-    Timer_ADC_Start();
+    AMUX_Start();            // Start the MUX
+    AMUX_FastSelect(PHOTOR); // Disconnect the last connection and connect the PHOTOR channel
+    ADC_DelSig_Start();      // Start the ADC
+    Timer_ADC_Start();       // Start the Timer
 }
 
-void Stop_HW_Components()
+void Stop_HW_Components()   // This function stops the hardware components: MUX, ADC and Timer
 {
-    Timer_ADC_Stop();
-    AMUX_DisconnectAll();
-    ADC_DelSig_Stop();
-    LED_PWM_Stop();
+    Timer_ADC_Stop();       // Stop the Timer
+    AMUX_DisconnectAll();   // Disconnect all the channel of the MUX
+    ADC_DelSig_Stop();      // Stop the ADC
+    LED_PWM_Stop();         // Stop the PWM associated to the Lamp_LED, therefore switching it off.
 }
 
-int32 AcquireData( )
+int32 AcquireData( )       // This function reads and returns the ADC result
 {
-    data_digit = ADC_DelSig_Read32();
+    read_data = ADC_DelSig_Read32();  // Save the ADC result in read_data variable
 
-    if( data_digit < 0) data_digit = 0;
-    else if (data_digit > 65535) data_digit= 65535;
+    // To be sure that read_data is in the range [0;65535]:
+    if( read_data < 0) read_data = 0;
+    else if (read_data > 65535) read_data= 65535;
 
-    return data_digit;
+    return read_data;  // Return the ADC result
 }
 
-void SwitchChannel()
+void SwitchChannel()  // This function switches channel of the MUX
 {
-    ADC_DelSig_StopConvert();
-    AMUX_FastSelect(Channel);
-    ADC_DelSig_StartConvert();  
+    // NOTE: since the ADC_DelSig Convertion Mode is set to 'Multi-sample', before switching the MUX channel
+    // it is necessary to stop the ADC convertion, otherwise the results could be contaminated.
+    
+    ADC_DelSig_StopConvert();  // Stop the ADC convertion
+    AMUX_FastSelect(Channel);  // Change the channel of the MUX (note: it is used FastSelect API since MUX AtMostOneActive is active )
+    ADC_DelSig_StartConvert(); // Start the ADC convertion
 }
 
+// Define Custom_UART_RX_ISR
 CY_ISR(Custom_UART_RX_ISR)
 {
-  ReceivedChar = UART_GetChar();
+  ReceivedChar = UART_GetChar();  // Save in ReceivedChar the last received byte of data.
       switch (ReceivedChar)
       {
         case 'B':
         case 'b':
-                Start_HW_Components();  
-                Communication_LED_PIN_Write(1);
+                Start_HW_Components();  // Starts the Hardware components; the sampling of the photoresistor signal starts.
+                Communication_LED_PIN_Write(1);  // Turn on the Communication_LED to warn that the Psoc is sending data.
                 break;
         case 'S':
         case 's':
-                Stop_HW_Components();
-                Communication_LED_PIN_Write(0);
+                Stop_HW_Components();  // Stops the Hardware components; the ADC stops to sample and convert and the Lamp_LED is switched off
+                Communication_LED_PIN_Write(0);  // Turn off the Communication_LED.
                 break;
         default: break;           
       }     
 }    
 
+// Define Custom_TIMER_ADC_ISR
 CY_ISR(Custom_TIMER_ADC_ISR)
 {
-    Timer_ADC_ReadStatusRegister();
-    if (!FlagAcquireData)
-        FlagAcquireData = 1;
+    Timer_ADC_ReadStatusRegister();  // Read Timer status register to bring interrupt line low
+    if (!FlagAcquireData)  // To be sure that the last value has been read (and so FlagAcquireData has been reset to 0 by the main )
+        FlagAcquireData = 1; 
 }
 
 /* [] END OF FILE */
--- BURCHIELLI_DAVIDE.cydsn/InterruptRoutines.h
+++ BURCHIELLI_DAVIDE.cydsn/InterruptRoutines.h
@@ -8,31 +8,33 @@
 #ifndef _INTERRUPT_ROUTINES_H
     #define _INTERRUPT_ROUTINES_H
     
-    // Define the 2 possible value of the MUX channel
-    #define POTENTIOMETER 0  // The MUX channel 0 is used to acquire the potentiometer signal
-    #define PHOTOR 1         // The MUX channel 1 is used to acquire the potentiometer signal
-    
-    #define BYTE_TO_SEND 4
-    #define TRANSMIT_BUFFER_SIZE 1 + BYTE_TO_SEND + 1
-    
     #include "project.h"
+    
+    // Create macro for the 2 MUX channel
+    #define POTENTIOMETER 0  // The MUX channel 0 is used to acquire the potentiometer signal
+    #define PHOTOR 1         // The MUX channel 1 is used to acquire the Photoresistor signal
+    
+    #define BYTE_TO_SEND 4   // Macro that identifies the number of bytes to be sent 
+    #define TRANSMIT_BUFFER_SIZE 1 + BYTE_TO_SEND + 1  // Macro that identifies the number of bytes to be sent including the HEADER and TAIL byte.
+                                                       // It identifies the dimention of the array DataBuffer
 
+    // Declare the 2 ISR
     CY_ISR_PROTO (Custom_TIMER_ADC_ISR);
     CY_ISR_PROTO (Custom_UART_RX_ISR);
     
+    // Declare the functions
     void Start_HW_Components();
     void Stop_HW_Components();
     void SwitchChannel();
     int32 AcquireData( );
        
-    uint8_t ReceivedChar;  // Define and initialize the variable ReceivedChar on which it is saved the byte recived
-    volatile _Bool Channel;
-    volatile _Bool FlagAcquireData;
-
-    int32 PotentValue, PhotoResValue;
-    uint8_t DataBuffer[TRANSMIT_BUFFER_SIZE];
-    int32 Threshold;
-    
-    
+    // Declare the variables 
+    uint8_t ReceivedChar;  // ReceivedChar variable contains the received byte of data
+    volatile _Bool Channel;  // Channel variable identidies the current channel of the MUX
+    volatile _Bool FlagAcquireData;  // FlagAcquireData is used to warn the main that a sample must be read by the ADC
+    int32 PotentValue;    // PotentValue variable contains the last value read by the ADC from the channel POTENTIOMETER of the MUX
+    int32 PhotoResValue;  // PhotoResValue variable contains the last value read by the ADC from the channel PHOTOR of the MUX
+    uint8_t DataBuffer[TRANSMIT_BUFFER_SIZE];  // DataBuffer array contains the bytes to be sent including the HEADER and TAIL byte
+ 
 #endif
 /* [] END OF FILE */
